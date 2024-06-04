4e2d23808e58c485387b55ff24fb64f2d6c7ff6c
mohitrawat1315@gmail.com <mohitrawat1315@gmail.com>
2024-06-04 20:45:00 +0530

    practice_1

---
+++ mohit/practice.py
@@ -0,0 +1,62 @@
+#want to show something as output : use print function
+#() bracket we call parentheses
+print("hello world")
+print("world") 
+
+###variable are place holder for storing data
+variable name cannot start with number (0-4)
+variable are case sensitive(age,Age and AGE all are different variable)
+variable name start with a letter or the underscore char. (a-z,A-Z,_(underscore))
+x(variable name)=12(value to be assigned)###
+
+a = 4
+print(a)
+
+#variable name can only contain alph numeric characters and underscore (a-z,A-Z,_(underscore))
+mohit_12 = 5 
+print(mohit_12)
+
+#3a=56  
+#variable name cannot start with number (0-4)
+
+#inbuilt keywords cannot use as variable
+help("keywords")
+#for=89
+#break=70
+
+#for comments we use hash(#)
+
+#data type in python
+#Intergers (-9,0,9)
+#Float     (-9.4,8.8)
+#Booleans  (True,False)
+#String    (any thing which is inside coths )
+#None
+
+#IF YOU WANT TO KNOW WHAT DATA TYPE IS THIS 
+#WE USE TYPE  FUNTION
+#Intergers 
+a = 42
+print(type(a))
+print(a)
+
+#Float  
+b = 2.3
+print(type(b))
+print(b)
+
+#Booleans
+c = True
+print(type(c))
+print(c)
+
+#String 
+d = "hello 3 @"
+print(type(d))
+print(d)
+
+#None
+n = None
+print(n)
+print(type(n))
+
