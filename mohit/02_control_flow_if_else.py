b4d5fb1c756608445d1a04f91d1db44379f2af44
mohitrawat1315@gmail.com <mohitrawat1315@gmail.com>
2024-06-05 18:38:24 +0530

    practice_1

---
+++ mohit/02_control_flow_if_else.py
@@ -0,0 +1,20 @@
+#control flow 
+
+
+a=int(input("enter your  age : "))
+print("your age is a",a)
+if(a>18):
+    print("you can drive")
+else:
+    print("you cannot drive")
+
+
+
+x= int(input("enter the temperature : "))
+if (x>40):
+     print("it's a hot day")
+elif(x<10):
+     print("it's a cold day")
+else :
+     print("it's normal day")
+     
